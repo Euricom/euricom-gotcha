dd6069722405d271c09994b5b28d5fd9eae37c6e
Niels Peeters <nielspeeters11@hotmail.com>
2021-06-18 17:51:46 +0200

    Beutify our gotcha feed, add toastr message to pwa

--- frontend/src/components/KilledBy.tsx
+++ frontend/src/components/KilledBy.tsx
@@ -1,16 +1,30 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 
 interface KilledByProps {
-  time:Date,
-  text: string | ReactNode
+  attempt: any
 }
 
-const KilledBy = ({time = new Date(), text}: KilledByProps) => {
+// We don't need a XX Mb lib to solve this yet :D
+const weekday=new Array(7);
+weekday[0]="Mon";
+weekday[1]="Tue";
+weekday[2]="Wed";
+weekday[3]="Thu";
+weekday[4]="Fri";
+weekday[5]="Sat";
+weekday[6]="Sun";
+
+const KilledBy = ({ attempt }: KilledByProps) => {
+  const approvedDate = new Date(attempt?.approveDate);
+  const [day, hours, minutes] = [approvedDate.getDay(), approvedDate.getHours(), approvedDate.getMinutes()]
   return (
-    <p className="font-medium">
-      <span className="text-gray-300 mr-2">{time.toLocaleTimeString()}:</span>
-      {text}
-    </p>
+    <div className="font-medium mb-8">
+      <div className="w-96 max-w-full rounded-xl mb-2">
+        <img className="rounded-xl" src={attempt?.imageUrl}alt={attempt?.target?.userName}/>
+      </div>
+      <span className="text-gray-400 mr-2">{`${weekday[day]} ${hours}:${minutes}`} :</span>
+      <p className="font-bold"><span className="text-red-500">{attempt?.target?.userName}</span> was found by <span className="text-green-500">{attempt?.killer?.userName}</span></p>
+    </div>
   )
 }
 
