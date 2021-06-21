e53e3d2cde394ee6d27a79f1f41b514110e42e6d
Niels Peeters <nielspeeters11@hotmail.com>
2021-06-21 17:00:22 +0200

    Fetch data after sign in no longer blocked

--- frontend/src/hooks/useUser.ts
+++ frontend/src/hooks/useUser.ts
@@ -1,15 +1,26 @@
-import { useQuery } from 'react-query';
-import axios from 'axios';
-import useLocalStorage from './useLocalStorage';
+import { useQuery } from "react-query";
+import axios from "axios";
+import useLocalStorage from "./useLocalStorage";
 
-export const getUser = async ({userName, email}: any) => {
-  const { data } = await axios.post(`/user`, {userName, email});
+export const getUser = async ({ userName, email }: any) => {
+  if (!(userName && email)) {
+  }
+  const { data } = await axios.post(`/user`, { userName, email });
   return data;
 };
 
-const useGetUser = () => {
-  const [account] = useLocalStorage("gotcha-account",'');
-  return useQuery('user', () => getUser({userName: account?.account?.name, email: account?.account?.username}), { enabled: !!account});
-}
+const useGetUser = (result: any) => {
+  const [account] = useLocalStorage("gotcha-account", "");
+  const data = result || account;
+  return useQuery(
+    "user",
+    () =>
+      getUser({
+        userName: data?.account?.name,
+        email: data?.account?.username,
+      }),
+    { enabled: !!data?.account }
+  );
+};
 
 export default useGetUser;
