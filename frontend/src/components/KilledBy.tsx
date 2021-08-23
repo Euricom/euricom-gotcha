2b8c8c4a0a76926f25145c894fa782d10f3ea690
Niels Peeters <nielspeeters11@hotmail.com>
2021-08-23 17:42:26 +0200

    Add question answer to the found feed

--- frontend/src/components/KilledBy.tsx
+++ frontend/src/components/KilledBy.tsx
@@ -19,11 +19,13 @@ const KilledBy = ({ attempt }: KilledByProps) => {
   const [day, hours, minutes] = [approvedDate.getDay(), approvedDate.getHours(), approvedDate.getMinutes()]
   return (
     <div className="font-medium mb-8">
+      <span className="text-gray-400 mr-2">{`${weekday[day]}, ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`}</span>
+      <p className="font-regular text-sm"><span className="text-red-500 font-bold">{attempt?.target?.userName}</span> was found by <span className="text-green-500 font-bold">{attempt?.killer?.userName}</span></p>
+      <p className="font-regular text-sm"><span className="font-bold text-base">Q: </span>{attempt?.target?.question}</p>
+      <p className="font-regular"><span className="font-bold text-base">A: </span>{attempt?.answer}</p>
       <div className="w-96 max-w-full rounded-xl mb-2">
         <img className="rounded-xl" src={attempt?.imageUrl}alt={attempt?.target?.userName}/>
       </div>
-      <span className="text-gray-400 mr-2">{`${weekday[day]}, ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`}</span>
-      <p className="font-bold"><span className="text-red-500">{attempt?.target?.userName}</span> was found by <span className="text-green-500">{attempt?.killer?.userName}</span></p>
     </div>
   )
 }
